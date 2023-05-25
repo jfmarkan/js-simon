61c14723d0f74e562bfda7cb6644a303491e2bdf
jfmarkan <jfmarkan@gmail.com>
2023-05-25 15:45:01 +0200

    Bult structure. get time and target time

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//First of all I want to get the time I am now at
+let thisMoment = Date();
+
+// Second, I need the date and time for tomorrow at 9:30 AM
+const nextClass = new Date(('May 26, 2023 09:30:00'));
+
