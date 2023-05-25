3dd79a72aaf63368b9a583668dba4e9432eb3cc6
jfmarkan <jfmarkan@gmail.com>
2023-05-25 15:45:21 +0200

    update thisMoment every second

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ let thisMoment = Date();
 // Second, I need the date and time for tomorrow at 9:30 AM
 const nextClass = new Date(('May 26, 2023 09:30:00'));
 
+setInterval (asTimeGoesBy, 1000);
+
+
+
+function asTimeGoesBy (){
+    console.log(thisMoment);
+    thisMoment = Date();
+}
