d376368e77959d254a24b7c13880f591fb18c93a
jfmarkan <jfmarkan@gmail.com>
2023-05-25 19:35:53 +0200

    added a second screen once the timer reaches 0

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-//First of all I want to get the time I am now at
-let thisMoment = Date();
+const nextClass = Date.parse('May 26, 2023 09:30:00');
+const countDown = new Date(nextClass).getTime()
+const clase = setInterval(function() {
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24
+        const thisMoment = new Date().getTime()
+        const timeLeft = countDown - thisMoment;
 
-// Second, I need the date and time for tomorrow at 9:30 AM
-const nextClass = new Date(('May 26, 2023 09:30:00'));
+        document.getElementById("days").innerText = Math.floor(timeLeft / (day)),
+        document.getElementById("hours").innerText = Math.floor((timeLeft % (day)) / (hour)),
+        document.getElementById("minutes").innerText = Math.floor((timeLeft % (hour)) / (minute)),
+        document.getElementById("seconds").innerText = Math.floor((timeLeft % (minute)) / second);
 
-setInterval (asTimeGoesBy, 1000);
-
-
-
-function asTimeGoesBy (){
-    console.log(thisMoment);
-    thisMoment = Date();
-}
+        if (timeLeft < 0) {
+            document.getElementById("headline").innerText = "It's coding time";
+            document.getElementById("countdown").classList.add('hidden');
+            document.getElementById("content").classList.remove('hidden');
+            clearInterval(clase);
+          }
+          //seconds
+        }, 0);
