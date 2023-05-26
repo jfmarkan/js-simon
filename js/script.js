f51370b131ce1722f67981bea16e5f561d42ca8e
jfmarkan <jfmarkan@gmail.com>
2023-05-26 10:05:35 +0200

    deleted const that wasn's used.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,27 @@
-const nextClass = Date.parse('May 26, 2023 09:30:00');
-const countDown = new Date(nextClass).getTime()
+const nextClass = Date.parse('May 26, 2023 19:57:00');
+
 const clase = setInterval(function() {
-        const second = 1000;
-        const minute = second * 60;
-        const hour = minute * 60;
-        const day = hour * 24
-        const thisMoment = new Date().getTime()
-        const timeLeft = countDown - thisMoment;
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const thisMoment = new Date().getTime();
+  const timeLeft = nextClass - thisMoment;
 
-        document.getElementById("days").innerText = Math.floor(timeLeft / (day)),
-        document.getElementById("hours").innerText = Math.floor((timeLeft % (day)) / (hour)),
-        document.getElementById("minutes").innerText = Math.floor((timeLeft % (hour)) / (minute)),
-        document.getElementById("seconds").innerText = Math.floor((timeLeft % (minute)) / second);
+  // Per ottimizzare un po' il codice e non perdersi in esso, utilizzare l'operatore resto (%)
+  // In questo modo trasformo solo ciò che avanza da giorni in ore, ciò che avanza da ore in minuti e ciò che avanza da minuti in secondi.
+  //Pertanto, il risultato stampato è in realtà una trasformazione del resto che utilizza le costanti definite sopra.
+  document.getElementById("days").innerText = Math.floor(timeLeft / (day)),
+  document.getElementById("hours").innerText = Math.floor((timeLeft % (day)) / (hour)),
+  document.getElementById("minutes").innerText = Math.floor((timeLeft % (hour)) / (minute)),
+  document.getElementById("seconds").innerText = Math.floor((timeLeft % (minute)) / (second));
 
-        if (timeLeft < 0) {
-            document.getElementById("headline").innerText = "It's coding time";
-            document.getElementById("countdown").classList.add('hidden');
-            document.getElementById("content").classList.remove('hidden');
-            clearInterval(clase);
-          }
-          //seconds
-        }, 0);
+  // Una volta arrivato al tempo definito nella variabile nextClass, estraggo il timer e mostro il messaggio nascosto e le icone.
+  if (timeLeft < 0) {
+    document.getElementById("headline").innerText = "It's coding time";
+    document.getElementById("countdown").classList.add('hidden');
+    document.getElementById("content").classList.remove('hidden');
+    clearInterval(clase);
+  }
+  
+  }, 1000);
